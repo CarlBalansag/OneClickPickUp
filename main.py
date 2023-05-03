14251d3db2fe68435c13ee22881dc4015014b78c
carl Balansag <carl@techccb.com>
2023-05-03 13:26:08 -0700

    added templates but having error with endblock

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+post = [
+    {
+        "name": "Jack Black",
+        "address": "123 street",
+        "amount_packages": "14"
+    },
+
+    {
+        "name": "Carl Black",
+        "address": "6420 Sun",
+        "amount_packages": 12
+    }
+]
+
 @app.route("/")
 def home():
-    return "Hello World! First page"
-
-@app.route("/<name>")
-def user(name):
-    return f'hello {name}'
+    return render_template('home.html', posts=post)
 
 @app.route("/admin")
 def admin():
@@ -16,7 +26,7 @@ def admin():
 
 @app.route("/about")
 def about():
-    return f'about page'
+    return render_template('about.html', title="about")
 
 if __name__ == "__main__":
     app.run()
